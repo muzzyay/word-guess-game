097c6347daff83ba8698f03ee9e788c96de2c5a3
muzzyay <muzzyay@gmail.com>
2019-01-16 15:19:50 -0700

    fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,51 @@
 // Library of countries
-var countries = [
-  "albania",
-  "algeria",
-  "belgium",
-  "belarus",
-  "croatia",
-  "denmark",
-  "england",
-  "finland",
-  "germany",
-  "holland",
-  "jamaica",
-  "morocco",
-  "nigeria",
-  "romania",
-  "senegal",
-  "ukraine",
-  "tunisia",
-  "vietnam",
-  "myanmar",
-  "ireland",
-  "iceland",
-  "hungary",
-  "prussia",
-  "georgia",
-  "estonia",
-  "ecuador",
-  "armenia"
-];
+var game = {
+  countries: [
+    "albania",
+    "algeria",
+    "belgium",
+    "belarus",
+    "croatia",
+    "denmark",
+    "england",
+    "finland",
+    "germany",
+    "holland",
+    "jamaica",
+    "morocco",
+    "nigeria",
+    "romania",
+    "senegal",
+    "ukraine",
+    "tunisia",
+    "vietnam",
+    "myanmar",
+    "ireland",
+    "iceland",
+    "hungary",
+    "prussia",
+    "georgia",
+    "estonia",
+    "ecuador",
+    "armenia"
+  ],
+  guessedLetters: [],
+  wins: 0,
+  guessesRemaining: 10,
+  letters: ["_ ", "_ ", "_ ", "_ ", "_ ", "_ ", "_ "],
 
-// Array of guessed letter
-var guessedLetters = [];
-// set variables for wins and remaining guess
-var wins = 0;
-var guessesRemaining = 10;
+  reset: function() {
+    game.letters = ["_ ", "_ ", "_ ", "_ ", "_ ", "_ ", "_ "];
+    pickedCountry =
+      game.countries[Math.floor(Math.random() * game.countries.length)];
+    console.log(pickedCountry);
+    game.guessesRemaining = 10;
+    game.guessedLetters = [];
+  }
+};
 
-// array for letters of picked country
-var letters = ["_ ", "_ ", "_ ", "_ ", "_ ", "_ ", "_ "];
-
-//randomly picked country variable
-var pickedCountry = countries[Math.floor(Math.random() * countries.length)];
-console.log(pickedCountry);
+var pickedCountry =
+  game.countries[Math.floor(Math.random() * game.countries.length)];
 
 // runs the code below when a key is pressed
 document.onkeyup = function(event) {
@@ -48,68 +53,57 @@ document.onkeyup = function(event) {
   console.log(userLetter);
 
   //   checks if the key is entered before if true exits the function
-  if (guessedLetters.includes(userLetter)) {
+  if (game.guessedLetters.includes(userLetter)) {
     return;
   }
 
-//   adds entered keys to guessed letters array
-  guessedLetters.push(userLetter);
-  console.log("You guessed: " + guessedLetters[guessedLetters.length - 1]);
+  //   adds entered keys to guessed letters array
+  game.guessedLetters.push(userLetter);
+  console.log(
+    "You guessed: " + game.guessedLetters[game.guessedLetters.length - 1]
+  );
 
   //checks if the letter is in the word and if true, reveals it
   if (pickedCountry.includes(userLetter)) {
     for (k = 0; k < pickedCountry.length; k++) {
       if (userLetter === pickedCountry[k]) {
-        letters[k] = userLetter;
+        game.letters[k] = userLetter;
       }
     }
   } else {
-    guessesRemaining--;
+    game.guessesRemaining--;
   }
 
-  
+  // checks if whole word is revealed or not, if true, alerts the user "You won" and resets the game
 
-  // checks if whole word is revealed or not, if true, resets the code
-  if (
-    letters[0] === pickedCountry[0] &&
-    letters[1] === pickedCountry[1] &&
-    letters[2] === pickedCountry[2] &&
-    letters[3] === pickedCountry[3] &&
-    letters[4] === pickedCountry[4] &&
-    letters[5] === pickedCountry[5] &&
-    letters[6] === pickedCountry[6]
-  ) {
-    wins++;
-
-    reset();
+  var wincheck = 0;
+  for (var i = 0; i < pickedCountry.length; i++) {
+    if (game.letters[i] === pickedCountry[i]) {
+      wincheck++;
+      if (wincheck === 7) {
+        alert("You Won");
+        game.wins++;
+        game.reset();
+      }
+    }
   }
 
-// checks if user out of guesses, if true, resets the code
-  if (guessesRemaining === 0) {
-    reset();
+  // checks if user out of guesses, if true, resets the game
+  if (game.guessesRemaining === 0) {
+    game.reset();
   }
-
 
   // adds texts to the HTML
-  document.getElementById("wins-text").innerHTML = wins;
-  document.getElementById("guessed-letters-text").innerHTML = guessedLetters;
-  document.getElementById("remaining-text").innerHTML = guessesRemaining;
+  document.getElementById("wins-text").innerHTML = game.wins;
+  document.getElementById("guessed-letters-text").innerHTML =
+    game.guessedLetters;
+  document.getElementById("remaining-text").innerHTML = game.guessesRemaining;
   document.getElementById("letters-text").innerHTML =
-    letters[0] +
-    letters[1] +
-    letters[2] +
-    letters[3] +
-    letters[4] +
-    letters[5] +
-    letters[6];
+    game.letters[0] +
+    game.letters[1] +
+    game.letters[2] +
+    game.letters[3] +
+    game.letters[4] +
+    game.letters[5] +
+    game.letters[6];
 };
-
-
-// a function to reset the code
-function reset() {
-  letters = ["_ ", "_ ", "_ ", "_ ", "_ ", "_ ", "_ "];
-  pickedCountry = countries[Math.floor(Math.random() * countries.length)];
-  console.log(pickedCountry);
-  guessesRemaining = 10;
-  guessedLetters = [];
-}
